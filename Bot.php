1ed2c467ed572815e73fdacea40f3adcd9c482d4
diamondnft12 <129858548+diamondnft12@users.noreply.github.com>
2024-05-26 05:59:35 +0700

    Create Bot.php

---
+++ Bot.php
@@ -0,0 +1,10 @@
+<?php
+// Isi dari build_index.php
+include 'build_index.php';
+
+// Isi dari shortlink_index.php
+include 'shortlink_index.php';
+
+// Isi dari testoke.php
+include 'testoke.php';
+?>
